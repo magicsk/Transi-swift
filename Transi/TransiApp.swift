a0d46a4c312e9e529e0f0173923c155c2f7e6a18
magicsk <magicsk@magicsk.eu>
2022-11-05 17:15:08 +0100

    Initial Commit

---
+++ Transi/TransiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransiApp.swift
+//  Transi
+//
+//  Created by magic_sk on 05/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct TransiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
