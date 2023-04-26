95fbfa3f846f18e6700c847ac70dc8d3d9e5d3a5
magicsk <magicsk@magicsk.eu>
2023-04-26 13:36:08 +0200

    feat: improved fuzzy search  (new lib & debounce)

--- Transi/StopListView.swift
+++ Transi/StopListView.swift
@@ -5,8 +5,9 @@
 //  Created by magic_sk on 15/01/2023.
 //
 
+import Combine
+import Fuse
 import SwiftUI
-import FuzzyFind
 
 struct StopListView: View {
     @State private var searchText = ""
@@ -14,15 +15,17 @@ struct StopListView: View {
     @Binding var isPresented: Bool
     @Binding var stopId: Int
     @State private var searchResults: [Stop]
+    let fuse = Fuse()
     var dataProvider: DataProvider
-    
+    let searchTextPublisher = PassthroughSubject<String, Never>()
+
     init(stopId: Binding<Int>, dataProviderProp: DataProvider, isPresented: Binding<Bool>) {
         _isPresented = isPresented
         _stopId = stopId
         dataProvider = dataProviderProp
         searchResults = dataProvider.stops
     }
-    
+
     var body: some View {
         NavigationView {
             List(searchResults) { stop in
@@ -31,29 +34,44 @@ struct StopListView: View {
                     Spacer()
                 }
             icon: {
-                StopListIcon(stop.type)
-            }
-            .contentShape(Rectangle())
-            .onTapGesture {
-                dataProvider.changeStop(stopId: stop.id ?? 0)
-                self.isPresented = false
-            }
+                    StopListIcon(stop.type)
+                }
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    dataProvider.changeStop(stopId: stop.id ?? 0)
+                    self.isPresented = false
+                }
             }
         }
         .searchable(text: $searchText)
         .disableAutocorrection(true)
         .onChange(of: searchText) { searchText in
-            
+            searchTextPublisher.send(searchText)
+        }
+        .onReceive(
+            searchTextPublisher
+                .debounce(for: .milliseconds(250), scheduler: DispatchQueue.main)
+        ) { _ in
             if searchText.isEmpty {
                 searchResults = dataProvider.stops
             } else {
                 DispatchQueue.global(qos: .userInteractive).async {
-                    self.searchResults = dataProvider.stops.filter {
-                        $0.id ?? 0 < 0 || bestMatch(query: searchText.folding(options: .diacriticInsensitive, locale: .current).lowercased(), input: $0.name?.lowercased().folding(options: .diacriticInsensitive, locale: .current) ?? "") != nil
+                    let pattern = fuse.createPattern(from: searchText.simplify())
+                    let scoredStops = dataProvider.stops
+                        .map { stop -> (Stop) in
+                            var newStop: Stop = stop
+                            let score = (stop.id ?? 0 < 0) ? -2 : fuse.search(pattern, in: stop.name?.simplify() ?? "")?.score
+                            newStop.score = score
+                            return newStop
+                        }
+                    self.searchResults = scoredStops.filter {
+                        $0.id ?? 0 < 0 || $0.score != nil
                     }
+                    .sorted(by: {
+                        $0.score ?? 0 < $1.score ?? 0
+                    })
                 }
             }
         }
-        
     }
 }
