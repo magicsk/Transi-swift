021f4fd023730ae8edd664f53d836970c787b8a0
magicsk <magicsk@magicsk.eu>
2023-06-04 22:34:59 +0200

    feat: set Table as default view

--- Transi/ContentView.swift
+++ Transi/ContentView.swift
@@ -9,24 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject private var dataProvider = DataProvider()
+    @State private var selection = 2
     
     var body: some View {
-        TabView {
+        TabView (selection: $selection) {
             TripPlannerView(dataProvider)
                 .tabItem {
                     Image(systemName: "tram")
                     Text("Trip planner")
-                }
+                }.tag(1)
             VirtualTableView(dataProvider)
                 .tabItem {
                     Image(systemName: "clock.arrow.2.circlepath")
                     Text("Virtual Table")
-                }
+                }.tag(2)
             TimetablesView()
                 .tabItem {
                     Image(systemName: "calendar")
                     Text("Timetables")
-                }
+                }.tag(3)
+        }.onAppear {
+            selection = 2
         }
     }
 }
